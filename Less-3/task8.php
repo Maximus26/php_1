743a9956358aaaa540fb1eb5272090eebbb79725
Maximus26 <zateh@yandex.ru>
2019-06-14 00:40:47 +0300

    решил проблему вывода сообщения, если отсутствует город на нужную букву в этом регионе

--- Less-3/task8.php
+++ Less-3/task8.php
@@ -7,13 +7,15 @@ $arr = [
 ];
 foreach ($arr as $region => $cities) {
 	echo $region . '<br>';
-	$city_display = '';
+	$city_display = [];
 	foreach ($cities as $city) {
 		if (mb_substr($city, 0, 1) == 'К') {
-			$city_display .= $city . ', ';
+			$city_display[] = $city;
 		}
 	}
-	echo substr($city_display, 0, -2) . '<br><br>';
+	if (empty($city_display)){
+		echo 'Пусто';
+	}
+	echo implode(', ', $city_display) . '<br><br>';
 }
-// а вот как вывести сообщение если в регионе нет городов на нужную букву, не допер
 ?>
