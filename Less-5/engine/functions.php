15f3a71ef9529e2ba3c63199850992eb69ce6e8d
Maximus26 <zateh@yandex.ru>
2019-06-19 23:26:23 +0300

    прочие функции

---
+++ Less-5/engine/functions.php
@@ -0,0 +1,87 @@
+<?php
+//замена окончаний
+function time_rus($n, $f1, $f2, $f3){
+	$n10 = $n % 10;
+
+	if($n >= 5 && $n < 20){
+		echo $n . ' ' . $f1;
+	} elseif($n10 == 1){
+		echo $n . ' ' . $f2;
+	} elseif($n10 >= 2 && $n10 <= 4){
+		echo $n . ' ' . $f3;
+	} else {
+		echo $n . ' ' . $f1;
+	}
+}
+
+//генерация меню
+function menu($arr){
+	$item = '';
+	foreach($arr as $value){
+		$item .= '<li><a href="' . $value['link'] . '">' . $value['title'] . '</a></li>';
+	}
+	$items = '<ul>' . $item . '</ul>';
+	return $items;
+}
+
+//генерация галереи
+/*function gallery($dir){
+	$images = scandir($dir);
+	$gal_img = '';
+	foreach ($images as $image){
+		if($image != '.' && $image != '..'){
+			$gal_img .= '<a href="'. $dir . '/' . $image . '" class="fancy" rel="gallery">
+						<img src="'. $dir . '/' . $image . '" alt="img">
+						<div class="hovered"></div>
+					 </a>';
+		}
+	}
+	$gal = '<div class="slide">' . $gal_img . '</div>';
+	return $gal;
+}*/
+
+//генерация галереи2
+function gallery($arr){
+	$gal_img = '';
+	foreach ($arr as $val){
+		$gal_img .= '<a href="galleryitem.php?id=' . $val['id'] . '" class="fancy" target="_blank">
+						<img src="'. $val['url'] . '" alt="' . $val['title'] . '">
+						<div class="hovered"></div>
+					 </a>';
+		}
+	$gal = '<div class="slide">' . $gal_img . '</div>';
+	return $gal;
+}
+
+//шаблонизатор
+function render($file, $variables = [])
+{
+	if (!is_file($file)) {
+		echo 'Template file "' . $file . '" not found';
+		exit();
+	}
+
+	if (filesize($file) === 0) {
+		echo 'Template file "' . $file . '" is empty';
+		exit();
+	}
+
+
+	$templateContent = file_get_contents($file);
+
+	if (empty($variables)) {
+		return $templateContent;
+	}
+
+	foreach ($variables as $key => $value) {
+		if (is_array($value)) {
+			continue;
+		}
+
+		$key = '{{' . strtoupper($key) . '}}';
+
+		$templateContent = str_replace($key, $value, $templateContent);
+	}
+
+	return $templateContent;
+}
