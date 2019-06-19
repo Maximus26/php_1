79c60b36a494981f0b6d941b4e5c803378ff473c
Maximus26 <zateh@yandex.ru>
2019-06-19 23:28:10 +0300

    вывод отдельной новости

---
+++ Less-5/public/newsItem.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/menu.php';
+$menu = menu($menu_arr);
+
+$id = isset($_GET['id']) ? $_GET['id'] : false;
+
+if(!$id) {
+	echo 'id не передан';
+	die;
+}
+
+$sql = "SELECT * FROM news WHERE id = $id";
+
+$newsItem = show($sql);
+if(!$newsItem) {
+	echo "404";
+	die;
+}
+if (empty($newsItem['image'])) {
+	$newsItem['image'] = 'img/no-image.jpeg';
+}
+echo render(TEMPLATES_DIR . 'news.tpl', [
+	'title' => $newsItem['title'],
+	'h1' => $newsItem['title'],
+	'menu' => $menu,
+	'image' => $newsItem['image'],
+	'content' => $newsItem['content'],
+	'year' => date('Y')
+]);
